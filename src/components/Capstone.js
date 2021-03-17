2fd75884351e55754a26db693a4f3436e78f4df9
hrlnsnchz <hrlnsnchz@gmail.com>
2021-03-17 10:28:58 -0500

    auth login, register setup. broke my initial capstone component

--- src/components/Capstone.js
+++ src/components/Capstone.js
@@ -1,11 +1,30 @@
-import React from "react"
-import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { userStorageKey } from "./auth/authSettings"
 
 
+// export const Capstone = () => (
+    //             <Route>
+    //                 <ApplicationViews />
+    //             </Route>      
+    // )
+<Route render={() => {
+    if (sessionStorage.getItem(userStorageKey)) {
+        return (
+            <>
+          //Components that are rendered when the user is authenticated go inside this React fragment
+        </>
+      )
+    } else {
+        return <Redirect to="/login" />;
+    }
+}} />
 
-export const Capstone = () => (
-            <Route>
-                <ApplicationViews />
-            </Route>      
-)
+<Route path="/login">
+  <Login />
+</Route>
+<Route path="/register">
+  <Register />
+</Route>
