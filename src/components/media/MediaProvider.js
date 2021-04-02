22bdb411fa8b93648f0854c5ad5f0dbe14bdda59
hrlnsnchz <hrlnsnchz@gmail.com>
2021-04-02 18:53:11 -0500

    Users can now search for movies and tv shows using the api data

--- src/components/media/MediaProvider.js
+++ src/components/media/MediaProvider.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react"
+import { testAPI } from "../../Settings"
 
 export const MediaContext = createContext()
 
@@ -7,23 +8,29 @@ export const MediaProvider = (props) => {
     const [media, setMedia] = useState([])
 
     const getMedia = () => {
-        return fetch("https://api.themoviedb.org/3/movie/popular?api_key=26c69b235f6f31e659dc584b010ef7f8&language=en-US&page=1")
+        return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${testAPI.apiKey}&language=en-US&page=1`)
         .then(res => res.json())
         .then(setMedia)
     }
 
     const getMediaById = (id) => {
-        return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=26c69b235f6f31e659dc584b010ef7f8&language=en-US`)
+        return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${testAPI.apiKey}&language=en-US`)
             .then(res => res.json())
     }
 
     const [ searchTerms, setSearchTerms ] = useState("")
 
+    const handleAPISearch = (event) => {
+        return fetch(`https://api.themoviedb.org/3/search/multi?api_key=${testAPI.apiKey}&query=${event}&language=en-US&page=1&include_adult=false`)
+          .then(res => res.json())
+          .then(setSearchTerms)
+      }
+
 
     return (
         <>
         <MediaContext.Provider value={{
-            media, getMedia, getMediaById, searchTerms, setSearchTerms
+            media, getMedia, getMediaById, searchTerms, setSearchTerms, handleAPISearch
         }}>
             {props.children}
         </MediaContext.Provider>
